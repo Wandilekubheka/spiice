aef636739cc736817a6016c91704432a5106ccbd
Wandile Kubheka <retroff01@gmail.com>
2025-06-07 11:42:59 +0200

    basic forgot passwrod ui

--- app/(auth)/forgotPass.tsx
+++ app/(auth)/forgotPass.tsx
@@ -1,14 +1,62 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Alert, TextInput, Button, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import usePassReset from "@/features/auth/hooks/usePassReset";
+import { AuthStatus } from "@/@types/authStatus";
+import { router } from "expo-router";
 
-type Props = {};
-
-const ForgotPassScreen = (props: Props) => {
+const ForgotPassScreen = () => {
+  const { error, sendPassReset, updatePassword, status } = usePassReset();
+  const [code, setCode] = useState("");
+  const [showCodeModal, setShowCodeModal] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  useEffect(() => {
+    if (status === AuthStatus.Error) {
+      Alert.alert(error);
+    } else if (status === AuthStatus.Success) {
+      Alert.alert("password reset successfully");
+      // move you back to login screen
+      router.replace("/(auth)");
+    } else if (status === AuthStatus.TwoStepRequired) {
+      setShowCodeModal(true);
+    }
+  }, [status]);
+  if (showCodeModal) {
+    return (
+      <View style={styles.container}>
+        <TextInput placeholder="Code" value={code} onChangeText={setCode} />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+        />
+        <Button
+          title="submit"
+          onPress={async () => {
+            await updatePassword(code, password);
+          }}
+        />
+      </View>
+    );
+  }
   return (
-    <View>
-      <Text>ForgotPassScreen</Text>
+    <View style={styles.container}>
+      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
+      <Button
+        title="submit"
+        onPress={async () => {
+          await sendPassReset(email);
+        }}
+      />
     </View>
   );
 };
 
 export default ForgotPassScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- features/auth/hooks/usePassReset.tsx
+++ features/auth/hooks/usePassReset.tsx
@@ -3,18 +3,13 @@ import React, { useState } from "react";
 import { useSignIn } from "@clerk/clerk-expo";
 import { AuthStatus } from "@/@types/authStatus";
 
-type Props = {
-  email: string;
-  password: string;
-};
-
-const usePassReset = ({ email, password }: Props) => {
+const usePassReset = () => {
   const { isLoaded, signIn, setActive } = useSignIn();
   const [status, setStatus] = useState<AuthStatus | null>(null);
   const [error, setError] = useState("");
 
   // Send the password reset code to the user's email
-  const sendPassReset = async () => {
+  const sendPassReset = async (email: string) => {
     if (!isLoaded) return;
     await signIn
       .create({
@@ -33,7 +28,7 @@ const usePassReset = ({ email, password }: Props) => {
   // Reset the user's password.
   // Upon successful reset, the user will be
   // signed in and redirected to the home page
-  const updatePassword = async (code: string) => {
+  const updatePassword = async (code: string, password: string) => {
     if (!isLoaded) return;
 
     await signIn
