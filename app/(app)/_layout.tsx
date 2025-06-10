4d7f9bdef2ebcfcbff88ac59eac559e99f6ac35a
Wandile Kubheka <retroff01@gmail.com>
2025-06-10 10:10:20 +0200

    app screens created

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,7 @@
+import { useAuth } from "@clerk/clerk-expo";
+import { Slot, Stack } from "expo-router";
+const AppLayout = () => {
+  return <Stack />;
+};
+
+export default AppLayout;
---
+++ app/(app)/messages.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const MessagesScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>MessagesScreen</Text>
+    </View>
+  );
+};
+
+export default MessagesScreen;
+Screen;
+const styles = StyleSheet.create({});
